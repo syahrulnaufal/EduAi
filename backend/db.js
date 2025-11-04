aefe08e790b32e2d6585b7486ab1fe0ba3bd08a1
naufal <syahrultsaqib@gmail.com>
2025-11-04 20:25:09 +0700

    UPDATE : update db.js untuk TIDB Cloud

--- backend/db.js
+++ backend/db.js
@@ -2,14 +2,29 @@
 import mysql from "mysql2/promise";
 
 // Buat koneksi langsung (ingat: ini async, jadi kita tunggu di awal aplikasi)
+// const db = await mysql.createConnection({
+//   host: 'localhost',
+//   user: 'root',
+//   password: '1234', // isi password kalau ada
+//   database: 'eduai'
+// });
+
+// // Tes koneksi
+// console.log('MySQL connected!');
+
+// export default db;
+
 const db = await mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '1234', // isi password kalau ada
-  database: 'eduai'
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME, 
+  port: process.env.DB_PORT || 4000,
+  ssl: { // SSL WAJIB untuk TiDB Cloud
+    rejectUnauthorized: true 
+  }
 });
 
-// Tes koneksi
-console.log('MySQL connected!');
+console.log('Terhubung ke TiDB Cloud (MySQL compatible)!');
 
 export default db;
